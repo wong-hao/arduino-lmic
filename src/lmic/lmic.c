34cebb35aeb5ae8973bb8a4c17ddf303226c498f
hwan0171 <hwan0171@student.monash.edu>
2021-11-09 16:07:53 +0800

    update

--- src/lmic/lmic.c
+++ src/lmic/lmic.c
@@ -2016,6 +2016,53 @@ static bit_t buildDataFrame (void) {
                     printf("\n"); 
 #endif
 
+#if LMIC_DEBUG_LEVEL > 0
+
+                    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+                    void lora_crc16(const char data, int *crc) {
+                        int next = 0;
+                        next  =  (((data>>0)&1) ^ ((*crc>>12)&1) ^ ((*crc>> 8)&1)                 )      ;
+                        next += ((((data>>1)&1) ^ ((*crc>>13)&1) ^ ((*crc>> 9)&1)                 )<<1 ) ;
+                        next += ((((data>>2)&1) ^ ((*crc>>14)&1) ^ ((*crc>>10)&1)                 )<<2 ) ;
+                        next += ((((data>>3)&1) ^ ((*crc>>15)&1) ^ ((*crc>>11)&1)                 )<<3 ) ;
+                        next += ((((data>>4)&1) ^ ((*crc>>12)&1)                                  )<<4 ) ;
+                        next += ((((data>>5)&1) ^ ((*crc>>13)&1) ^ ((*crc>>12)&1) ^ ((*crc>> 8)&1))<<5 ) ;
+                        next += ((((data>>6)&1) ^ ((*crc>>14)&1) ^ ((*crc>>13)&1) ^ ((*crc>> 9)&1))<<6 ) ;
+                        next += ((((data>>7)&1) ^ ((*crc>>15)&1) ^ ((*crc>>14)&1) ^ ((*crc>>10)&1))<<7 ) ;
+                        next += ((((*crc>>0)&1) ^ ((*crc>>15)&1) ^ ((*crc>>11)&1)                 )<<8 ) ;
+                        next += ((((*crc>>1)&1) ^ ((*crc>>12)&1)                                  )<<9 ) ;
+                        next += ((((*crc>>2)&1) ^ ((*crc>>13)&1)                                  )<<10) ;
+                        next += ((((*crc>>3)&1) ^ ((*crc>>14)&1)                                  )<<11) ;
+                        next += ((((*crc>>4)&1) ^ ((*crc>>15)&1) ^ ((*crc>>12)&1) ^ ((*crc>> 8)&1))<<12) ;
+                        next += ((((*crc>>5)&1) ^ ((*crc>>13)&1) ^ ((*crc>> 9)&1)                 )<<13) ;
+                        next += ((((*crc>>6)&1) ^ ((*crc>>14)&1) ^ ((*crc>>10)&1)                 )<<14) ;
+                        next += ((((*crc>>7)&1) ^ ((*crc>>15)&1) ^ ((*crc>>11)&1)                 )<<15) ;
+                        (*crc) = next;
+                    }
+
+                    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+                    uint16_t sx1302_lora_payload_crc(const uint8_t * data, uint8_t size) {
+                        int i;
+                        int crc = 0;
+
+                        for (i = 0; i < size; i++) {
+                            lora_crc16(data[i], &crc);
+                        }
+
+                        //printf("CRC16: 0x%02X 0x%02X (%X)\n", (uint8_t)(crc >> 8), (uint8_t)crc, crc);
+                        return (uint16_t)crc;
+                    }
+
+
+                    uint16_t payload_crc16_calc;
+
+                    payload_crc16_calc = sx1302_lora_payload_crc(LMIC.frame, LMIC.dataLen);
+                    LMIC_DEBUG_PRINTF("%"LMIC_PRId_ostime_t": Payload CRC Hex (0x%04X), Payload CRC DEC (%u)\n ",os_getTime(), payload_crc16_calc, payload_crc16_calc);
+
+#endif
+
     return 1;
 }
 
