a392b461ec0344ad494ec945c1295bbb7f9b8cfd
hwan0171 <hwan0171@student.monash.edu>
2021-11-20 16:13:16 +0800

    update

--- src/lmic/lmic.c
+++ src/lmic/lmic.c
@@ -32,8 +32,6 @@
 #define LMIC_DR_LEGACY 0
 #include "lmic_bandplan.h"
 
-#include "../payload_crc.h"
-
 #if defined(DISABLE_BEACONS) && !defined(DISABLE_PING)
 #error Ping needs beacon tracking
 #endif
@@ -2035,27 +2033,27 @@ static bit_t buildDataFrame (void) {
                        memcpy(&e_.opts[0], LMIC.frame+LORA::OFF_DAT_OPTS, end-LORA::OFF_DAT_OPTS)));
     LMIC.dataLen = flen;
     
+    printf("Original sent %d Byte Payload: ", LMIC.dataLen);
     for (int loopcount = 0; loopcount < LMIC.dataLen; loopcount++) {
-        LMIC.originalframe[loopcount] = LMIC.frame[loopcount];
+        printf("%02X", LMIC.frame[LMIC.dataBeg + loopcount]);
     }
+    printf("\n");
+
+    u2_t payload_crc16_calc;
+    payload_crc16_calc = sx1302_lora_payload_crc(LMIC.frame, LMIC.dataLen);
+    printf("Orignal Payload CRC Hex (0x%04X), Payload CRC DEC (%u)\n", payload_crc16_calc, payload_crc16_calc);
 
     switch (StageOption) {
         case 0: { //不作处理
             break;
         }
         case 1: { //CRC插到PHY Payload最后
-            u2_t payload_crc16_calc;
-            payload_crc16_calc = sx1302_lora_payload_crc(LMIC.frame, LMIC.dataLen);
-
             LMIC.frame[LMIC.dataLen] = payload_crc16_calc >> 8; //https://stackoverflow.com/a/1289360/12650926
             LMIC.frame[LMIC.dataLen + 1] = payload_crc16_calc & 0xff;
             LMIC.dataLen = LMIC.dataLen + 2;
             break;
         }
         case 2: { //CRC插到PHY Payload最前
-            u2_t payload_crc16_calc;
-            payload_crc16_calc = sx1302_lora_payload_crc(LMIC.frame, LMIC.dataLen);
-
             u2_t frame_copy[MAX_LEN_FRAME];
             frame_copy[0] = payload_crc16_calc >> 8;
             frame_copy[1] = payload_crc16_calc & 0xff;
